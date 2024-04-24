4481c6afa505809413e795f764d0e03a887f2211
Andrew Pogrebnoy <absourd.noise@gmail.com>
2024-04-24 19:59:14 +0300

    Make XLog storage extensible
    and allow extensions to override it
    For now, it extends on `pread` and `pwrite` from/into segment files.
    This is the minimum we need for full XLog encryption with pg_de.

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -61,6 +61,7 @@
 #include "access/xloginsert.h"
 #include "access/xlogreader.h"
 #include "access/xlogrecovery.h"
+#include "access/xlog_smgr.h"
 #include "access/xlogutils.h"
 #include "backup/basebackup.h"
 #include "catalog/catversion.h"
@@ -2432,7 +2433,7 @@ XLogWrite(XLogwrtRqst WriteRqst, TimeLineID tli, bool flexible)
 				start = pgstat_prepare_io_time(track_wal_io_timing);
 
 				pgstat_report_wait_start(WAIT_EVENT_WAL_WRITE);
-				written = pg_pwrite(openLogFile, from, nleft, startoffset);
+				written = xlog_smgr->seg_write(openLogFile, from, nleft, startoffset, tli, openLogSegNo, wal_segment_size);
 				pgstat_report_wait_end();
 
 				pgstat_count_io_op_time(IOOBJECT_WAL, IOCONTEXT_NORMAL,
@@ -3422,6 +3423,7 @@ XLogFileCopy(TimeLineID destTLI, XLogSegNo destsegno,
 	int			srcfd;
 	int			fd;
 	int			nbytes;
+	off_t		offset = 0;
 
 	/*
 	 * Open the source file
@@ -3470,7 +3472,8 @@ XLogFileCopy(TimeLineID destTLI, XLogSegNo destsegno,
 			if (nread > sizeof(buffer))
 				nread = sizeof(buffer);
 			pgstat_report_wait_start(WAIT_EVENT_WAL_COPY_READ);
-			r = read(srcfd, buffer.data, nread);
+			r = xlog_smgr->seg_read(srcfd, buffer.data, nread, offset,
+ 					srcTLI, srcsegno, wal_segment_size);
 			if (r != nread)
 			{
 				if (r < 0)
@@ -3488,7 +3491,7 @@ XLogFileCopy(TimeLineID destTLI, XLogSegNo destsegno,
 		}
 		errno = 0;
 		pgstat_report_wait_start(WAIT_EVENT_WAL_COPY_WRITE);
-		if ((int) write(fd, buffer.data, sizeof(buffer)) != (int) sizeof(buffer))
+		if ((int) xlog_smgr->seg_write(fd, buffer.data, sizeof(buffer), offset, destTLI, destsegno, wal_segment_size) != (int) sizeof(buffer))
 		{
 			int			save_errno = errno;
 
@@ -3504,6 +3507,7 @@ XLogFileCopy(TimeLineID destTLI, XLogSegNo destsegno,
 					 errmsg("could not write to file \"%s\": %m", tmppath)));
 		}
 		pgstat_report_wait_end();
+		offset += sizeof(buffer);
 	}
 
 	pgstat_report_wait_start(WAIT_EVENT_WAL_COPY_SYNC);
