8e12b23af6d39a4b29ded0c99914e1b59f2739d5
Andrew Pogrebnoy <absourd.noise@gmail.com>
2024-04-24 19:59:14 +0300

    Make XLog storage extensible
    and allow extensions to override it
    For now, it extends on `pread` and `pwrite` from/into segment files.
    This is the minimum we need for full XLog encryption with pg_de.

---
+++ src/include/access/xlog_smgr.h
@@ -0,0 +1,41 @@
+#ifndef XLOG_SMGR_H
+#define XLOG_SMGR_H
+
+#include "postgres.h"
+
+#include <unistd.h>
+
+/* XLog storage manager interface */
+typedef struct XLogSmgr
+{
+	ssize_t		(*seg_read) (int fd, void *buf, size_t count, off_t offset,
+							 TimeLineID tli, XLogSegNo segno, int segSize);
+
+	ssize_t		(*seg_write) (int fd, const void *buf, size_t count, off_t offset,
+							  TimeLineID tli, XLogSegNo segno, int segSize);
+} XLogSmgr;
+
+static inline ssize_t
+default_seg_write(int fd, const void *buf, size_t count, off_t offset,
+				  TimeLineID tli, XLogSegNo segno, int segSize)
+{
+	return pg_pwrite(fd, buf, count, offset);
+}
+
+static inline ssize_t
+default_seg_read(int fd, void *buf, size_t count, off_t offset,
+				 TimeLineID tli, XLogSegNo segno, int segSize)
+{
+	return pg_pread(fd, buf, count, offset);
+}
+
+/* Default (standard) XLog storage manager */
+static const XLogSmgr xlog_smgr_standard = {
+	.seg_read = default_seg_read,
+	.seg_write = default_seg_write,
+};
+
+extern const XLogSmgr *xlog_smgr;
+extern void SetXLogSmgr(const XLogSmgr *xlsmgr);
+
+#endif							/* XLOG_SMGR_H */
