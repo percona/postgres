9bd71a588a505ac08a8c62ca40b8c2c60506e4cb
Andrew Pogrebnoi <absourd.noise@gmail.com>
2025-03-21 21:28:52 +0200

    Pass proper old rlocator to smgr when creating relations and indexes (#156)

--- src/backend/commands/indexcmds.c
+++ src/backend/commands/indexcmds.c
@@ -1269,7 +1269,7 @@ DefineIndex(Oid tableId,
 					 coloptions, NULL, reloptions,
 					 flags, constr_flags,
 					 allowSystemTableMods, !check_rights,
-					 &createdConstraintId);
+					 &createdConstraintId, NULL);
 
 	ObjectAddressSet(address, RelationRelationId, indexRelationId);
 
