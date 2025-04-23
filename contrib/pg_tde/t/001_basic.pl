856e6e859491a4c0a8917a0b53ac0730eba5cc9a
Anders Åstrand <anders.astrand@ext.percona.com>
2025-04-23 09:03:18 +0200

    Reformat SQL to make it easier to read
    Just because these are tests we don't have to make the SQL hard to read.

--- contrib/pg_tde/t/001_basic.pl
+++ contrib/pg_tde/t/001_basic.pl
@@ -13,22 +13,46 @@ $node->append_conf('postgresql.conf', "shared_preload_libraries = 'pg_tde'");
 $node->start;
 
 $node->safe_psql('postgres', q{CREATE EXTENSION IF NOT EXISTS pg_tde});
-$node->safe_psql('postgres',
-	q{SELECT pg_tde_add_database_key_provider_file('file-vault','/tmp/pg_tde_test_keyring.per')}
+$node->safe_psql(
+	'postgres', q{
+		SELECT pg_tde_add_database_key_provider_file(
+			provider_name => 'file-vault',
+			file_path => '/tmp/pg_tde_test_keyring.per'
+		)
+	}
 );
-$node->safe_psql('postgres',
-	q{SELECT pg_tde_set_key_using_database_key_provider('test-db-key','file-vault')}
+$node->safe_psql(
+	'postgres', q{
+		SELECT pg_tde_set_key_using_database_key_provider(
+			key_name => 'test-db-key',
+			provider_name => 'file-vault'
+		)
+	}
 );
-$node->safe_psql('postgres',
-	q{CREATE TABLE test_enc(id SERIAL,k VARCHAR(32),PRIMARY KEY (id)) USING tde_heap}
+$node->safe_psql(
+	'postgres', q{
+		CREATE TABLE test_enc(
+			id SERIAL,
+			k VARCHAR(32),
+			PRIMARY KEY (id)
+		) USING tde_heap
+	}
 );
-$node->safe_psql('postgres',
-	q{INSERT INTO test_enc (k) VALUES ('foobar'),('barfoo')});
+$node->safe_psql(
+	'postgres', q{
+		INSERT INTO test_enc (k) VALUES ('foobar'), ('barfoo')
+	}
+);
 
 $node->restart;
 
-is($node->safe_psql('postgres', q{SELECT * FROM test_enc ORDER BY id ASC}),
-	"1|foobar\n2|barfoo", 'tde_heap table can be read after server restart');
+is( $node->safe_psql(
+		'postgres', q{
+			SELECT * FROM test_enc ORDER BY id ASC
+		}
+	),
+	"1|foobar\n2|barfoo",
+	'tde_heap table can be read after server restart');
 
 my $tablefile = $node->data_dir . '/'
   . $node->safe_psql('postgres', q{SELECT pg_relation_filepath('test_enc')});
