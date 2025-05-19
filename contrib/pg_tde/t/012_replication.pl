8cc554f2fba8bb1cc04b8d39775d027e3b5ffe14
Anders Åstrand <anders.astrand@ext.percona.com>
2025-05-19 19:13:12 +0200

    Sleep after killing server in replication test
    This test often failed in CI, and I believe the cause is that we tried
    to start the cluster again before it had been fully killed.

--- contrib/pg_tde/t/012_replication.pl
+++ contrib/pg_tde/t/012_replication.pl
@@ -75,6 +75,8 @@ PGTDE::psql($primary, 'postgres',
 PGTDE::psql($primary, 'postgres',
 	"ALTER SYSTEM SET pg_tde.wal_encrypt = 'on';");
 $primary->kill9;
+# Sleep to ensure we don't try to start the server again before it's been properly killed.
+sleep(1);
 
 PGTDE::append_to_result_file("-- primary start");
 $primary->start;
