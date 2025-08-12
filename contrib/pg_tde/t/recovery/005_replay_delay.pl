be1abb0d0bf564bced4b8e3127798cdf895499ab
Shahid Ullah <shahidullah.khan@percona.com>
2025-08-13 04:03:27 +0500

    Update testcases with new helper function.

--- contrib/pg_tde/t/recovery/005_replay_delay.pl
+++ contrib/pg_tde/t/recovery/005_replay_delay.pl
@@ -14,38 +14,7 @@ use pgtde;
 # Initialize primary node
 my $node_primary = PostgreSQL::Test::Cluster->new('primary');
 $node_primary->init(allows_streaming => 1);
-$node_primary->append_conf('postgresql.conf',
-	"shared_preload_libraries = 'pg_tde'");
-$node_primary->append_conf('postgresql.conf',
-	"default_table_access_method = 'tde_heap'");
-$node_primary->start;
-
-unlink('/tmp/global_keyring.file');
-unlink('/tmp/local_keyring.file');
-# Create and enable tde extension
-$node_primary->safe_psql('postgres', 'CREATE EXTENSION IF NOT EXISTS pg_tde;');
-$node_primary->safe_psql('postgres',
-	"SELECT pg_tde_add_global_key_provider_file('global_key_provider', '/tmp/global_keyring.file');");
-$node_primary->safe_psql('postgres',
-	"SELECT pg_tde_create_key_using_global_key_provider('global_test_key_delay', 'global_key_provider');");
-$node_primary->safe_psql('postgres',
-	"SELECT pg_tde_set_server_key_using_global_key_provider('global_test_key_delay', 'global_key_provider');");
-$node_primary->safe_psql('postgres',
-	"SELECT pg_tde_add_database_key_provider_file('local_key_provider', '/tmp/local_keyring.file');");
-$node_primary->safe_psql('postgres',
-	"SELECT pg_tde_create_key_using_database_key_provider('local_test_key_delay', 'local_key_provider');");
-$node_primary->safe_psql('postgres',
-	"SELECT pg_tde_set_key_using_database_key_provider('local_test_key_delay', 'local_key_provider');");
-
-my $WAL_ENCRYPTION = $ENV{WAL_ENCRYPTION} // 'on';
-
-$node_primary->append_conf(
-    'postgresql.conf',
-    ($WAL_ENCRYPTION eq 'off')
-        ? "pg_tde.wal_encrypt = off\n"
-        : "pg_tde.wal_encrypt = on\n"
-);
-
+PGTDE::setup_pg_tde_node($node_primary);
 $node_primary->restart;
 
 # And some content
