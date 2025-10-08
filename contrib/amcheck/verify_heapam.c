95063fde92b947e784e00fa401671aa942f9bb21
Andreas Karlsson <andreas.karlsson@percona.com>
2025-10-08 18:33:05 +0200

    Make check in heap AM related extensions support everything heap-like
    Instead of checking for the oid of the AM we check for if the
    access method of the relation uses the routine struct as the
    heap does. This allows the tools to be support all access
    methods which are simple copies of heap.
    
    I am uncertain if this is something which the project would be
    interested in but it decreases the size of our diff against
    upstream.

--- contrib/amcheck/verify_heapam.c
+++ contrib/amcheck/verify_heapam.c
@@ -16,6 +16,7 @@
 #include "access/multixact.h"
 #include "access/relation.h"
 #include "access/table.h"
+#include "access/tableam.h"
 #include "access/toast_internals.h"
 #include "access/visibilitymap.h"
 #include "access/xact.h"
@@ -343,7 +344,7 @@ verify_heapam(PG_FUNCTION_ARGS)
 	 * Other relkinds might be using a different AM, so check.
 	 */
 	if (ctx.rel->rd_rel->relkind != RELKIND_SEQUENCE &&
-		ctx.rel->rd_rel->relam != HEAP_TABLE_AM_OID)
+		ctx.rel->rd_tableam != GetHeapamTableAmRoutine())
 		ereport(ERROR,
 				(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
 				 errmsg("only heap AM is supported")));
